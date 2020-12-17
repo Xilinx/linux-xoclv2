b225a3ae7309248635c4d0a4101f018bf98d2a21
Sonal Santan <sonal.santan@xilinx.com>
2020-12-16 22:25:01 -0800

    fpga: xrt: infrastructure support for xmgmt driver
    Add infrastructure code for XRT management physical function
    driver. This provides support for enumerating and extracting
    sections from xclbin files, interacting with device tree nodes
    found in xclbin and working with Alveo partitions.
    
    Signed-off-by: Sonal Santan <sonal.santan@xilinx.com>

---
+++ drivers/fpga/xrt/common/xrt-root.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2020 Xilinx, Inc.
+ *
+ * Authors:
+ *	Cheng Zhen <maxz@xilinx.com>
+ */
+
+#ifndef	_XRT_ROOT_H_
+#define	_XRT_ROOT_H_
+
+#include <linux/pci.h>
+#include "subdev.h"
+
+struct xroot;
+
+int xroot_probe(struct pci_dev *pdev, struct xroot **root);
+void xroot_remove(struct xroot *root);
+bool xroot_wait_for_bringup(struct xroot *root);
+int xroot_add_vsec_node(struct xroot *root, char *dtb);
+int xroot_create_partition(struct xroot *xr, char *dtb);
+int xroot_add_simple_node(struct xroot *root, char *dtb, const char *endpoint);
+void xroot_hot_reset(struct pci_dev *pdev);
+void xroot_broadcast(struct xroot *root, enum xrt_events evt);
+
+#endif	/* _XRT_ROOT_H_ */
